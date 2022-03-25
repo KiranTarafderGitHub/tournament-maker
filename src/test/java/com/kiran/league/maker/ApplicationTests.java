fe5f4fd26d6d8009be2b77eff27c87766448155c
Kiran <kiran_tarafder@msrlimited.com>
2022-03-25 17:11:12 +0530

    first checkin

---
+++ src/test/java/com/kiran/league/maker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.league.maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
