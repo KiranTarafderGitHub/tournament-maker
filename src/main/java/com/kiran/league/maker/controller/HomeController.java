4bd5cfff9a601638dc5c43e2984b8f5cc95cb148
Kiran <kiran_tarafder@msrlimited.com>
2022-03-26 17:10:28 +0530

    Home Controller

--- src/main/java/com/kiran/league/maker/controller/HomeController.java
+++ src/main/java/com/kiran/league/maker/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.kiran.league.maker.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
